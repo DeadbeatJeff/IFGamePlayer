1ee09b0b9a2bfa47041c36dac63629600f8b2eca
Jeff Rolland <DeadbeatJeffSDF@gmail.com>
2026-08-16 08:57:07 -0400

    Fixed some problems with main.py.

--- main.py
+++ main.py
@@ -291,4 +291,4 @@ if __name__ == "__main__":
         try:
             play_walkthrough(env, q_net, encoder, device, max_steps=args.walkthrough_steps)
         finally:
-        k    env.close()
+            env.close()
